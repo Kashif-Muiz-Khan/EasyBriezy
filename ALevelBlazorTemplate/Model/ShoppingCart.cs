b9bdddfd0bd7dc0c0cf4a1d7d2fb20b48eb747a9
Kashif-Muiz-Khan <khankashifmuiz@gmail.com>
2024-04-25 09:33:25 +0100

    Improvement on StoreItem - (need to add cart item)

--- ALevelBlazorTemplate/Model/ShoppingCart.cs
+++ ALevelBlazorTemplate/Model/ShoppingCart.cs
@@ -5,36 +5,45 @@
         public event Action? OnCartUpdated;
         private List<CartItem> _items;
 
-        public ShoppingCart() 
+        public ShoppingCart()
         {
             _items = [];
         }
 
-
-
-        public IEnumerable<CartItem> GetItems()
-        {
-            return _items;
-        }
-
         public void AddItem(Cheese cheese, int quantity)
         {
+            // add the cheese or increase the quantity
             var item = _items.FirstOrDefault(item => item.Cheese.Id == cheese.Id);
-            if (item == null)
-            {
+            if (item is null)
                 _items.Add(new CartItem { Cheese = cheese, Quantity = quantity });
-            }
             else
-            {
                 item.Quantity += quantity;
-            }
 
+            // alert the listeners to the cart change
             OnCartUpdated?.Invoke();
         }
 
+        public int Count()
+        {
+            // return the number of items in the cart
+            return _items.Count;
+        }
+
+        public decimal Total()
+        {
+            // sum the price of all items in the cart
+            return _items.Sum(item => item.Cheese.Price * item.Quantity);
+        }
+
+        public IEnumerable<CartItem> GetItems()
+        {
+            // return a copy of the items in the cart
+            return _items;
+        }
 
         public void RemoveItem(Cheese cheese)
         {
+            // remove the cheese from the cart
             _items.RemoveAll(item => item.Cheese.Id == cheese.Id);
             OnCartUpdated?.Invoke();
         }
@@ -51,12 +60,6 @@
             OnCartUpdated?.Invoke();
         }
 
-        public decimal Total()
-        {
-            // sum the price of all items in the cart
-            return _items.Sum(item => item.Cheese.Price * item.Quantity);
-        }
-
         public void Clear()
         {
             // remove all items from the cart
@@ -64,11 +67,18 @@
             OnCartUpdated?.Invoke();
         }
 
+        public int GetQuantity(Cheese cheese)
+        {
+            // return the quantity of the cheese in the cart
+            var item = _items.FirstOrDefault(item => item.Cheese.Id == cheese.Id);
+            return item?.Quantity ?? 0;
+        }
+
         public void SetItems(IEnumerable<CartItem> items)
         {
+            // set the items in the cart
             _items = items.ToList();
             OnCartUpdated?.Invoke();
         }
-
     }
 }
